bc8e6b8050304fd047c08ba7feb163c2e7de9627
Daniele Biagetti <daniele.biagetti@cblelectronics.com>
2018-09-14 16:44:29 +0200

    Aligned my board files with the current upstream/master

--- boards/arm/acn52832/board.h
+++ boards/arm/acn52832/board.h
@@ -9,19 +9,4 @@
 
 #include <soc.h>
 
-/* Onboard RED LED */
-#define LED0_GPIO_PIN  22
-#define LED0_GPIO_PORT CONFIG_GPIO_P0_DEV_NAME
-
-/* Onboard BLUE LED */
-#define LED1_GPIO_PIN  23
-#define LED1_GPIO_PORT CONFIG_GPIO_P0_DEV_NAME
-
-/* Onboard GREEN LED */
-#define LED2_GPIO_PIN  24
-#define LED2_GPIO_PORT CONFIG_GPIO_P0_DEV_NAME
-
-#define LED_ON  0
-#define LED_OFF 1
-
 #endif /* __INC_BOARD_H */
